8badc274bbe1f7d079e9385012a21b50ce9b7160
João Trindade <joao.trindade@isel.pt>
2023-04-13 08:04:09 +0100

    Lecture 17

--- 17-code-generation/01-in-memory/codegen/src/main/kotlin/pt/isel/leic/lae/codegen/CodeGen.kt
+++ 17-code-generation/01-in-memory/codegen/src/main/kotlin/pt/isel/leic/lae/codegen/CodeGen.kt
@@ -3,6 +3,20 @@ package pt.isel.leic.lae.codegen
 import java.io.*
 import org.cojen.maker.*
 
+/*
+ * Basic example taken from Cojen/Maker documentation.
+ * Creates a class with a generated name and a single static method, run:
+ * 
+ * public class #SomeName# {
+ *   public static void run()
+ *     System.out.println("Hello, generated world!");
+ *   }
+ * }
+ *
+ * The static method run is then invoked via reflection.
+ *
+ */
+
 fun main() {
 	val cm : ClassMaker = ClassMaker.begin().public_()
 
